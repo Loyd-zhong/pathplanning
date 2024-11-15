305edffe33820c65cab1ea857a634f104e69a686
Loyd-zhong <15360415069@163.com>
2024-11-15 16:13:57 +0800

    更新了数据库逻辑，但没尽善尽美

---
+++ src/main/java/com/enterprise/common/models/PassRecord.java
@@ -0,0 +1,55 @@
+// PassRecord.java
+package com.enterprise.common.models;
+
+import java.time.LocalDateTime;
+
+public class PassRecord {
+    private final String identifier; // 标识符（节点或边的标识）
+    private int count; // 通过次数
+    private LocalDateTime lastPassTime; // 最近一次通过时间
+
+    // 构造方法，接受 Edge 类型
+    public PassRecord(Edge edge) {
+        this.identifier = "Edge-" + edge.getFrom().getX() + "," + edge.getFrom().getY() +
+                "->" + edge.getTo().getX() + "," + edge.getTo().getY();
+        this.count = 0;
+        this.lastPassTime = LocalDateTime.now();
+    }
+
+    // 构造方法，接受 Node 类型
+    public PassRecord(Node node) {
+        this.identifier = "Node-" + node.getX() + "," + node.getY();
+        this.count = 0;
+        this.lastPassTime = LocalDateTime.now();
+    }
+    public PassRecord(String identifier) {
+        this.identifier = identifier;
+        this.count = 0;
+        this.lastPassTime = LocalDateTime.now();
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void incrementCount() {
+        this.count++;
+    }
+
+    public LocalDateTime getLastPassTime() {
+        return lastPassTime;
+    }
+
+    public void setLastPassTime(LocalDateTime lastPassTime) {
+        this.lastPassTime = lastPassTime;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + identifier + ", Count: " + count + ", Last Pass: " + lastPassTime;
+    }
+}
---
+++ src/main/java/com/enterprise/common/utils/MapLoader.java
@@ -0,0 +1,86 @@
+package com.enterprise.common.utils;
+
+import com.enterprise.common.models.Graph;
+import com.enterprise.common.models.Node;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapLoader {
+    public static Graph loadMap(String filePath) {
+        Graph graph = new Graph();
+        Map<String, Node> nodeMap = new HashMap<>();
+        
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new File(filePath));
+            
+            // 第一遍：创建所有节点
+            NodeList pointList = document.getElementsByTagName("PointInfo");
+            for (int i = 0; i < pointList.getLength(); i++) {
+                Element element = (Element) pointList.item(i);
+                String id = element.getElementsByTagName("id").item(0).getTextContent();
+                double xpos = Double.parseDouble(element.getElementsByTagName("xpos").item(0).getTextContent());
+                double ypos = Double.parseDouble(element.getElementsByTagName("ypos").item(0).getTextContent());
+                
+                Node node = new Node(xpos, ypos, id);
+                graph.addNode(node);
+                nodeMap.put(id, node);
+                //System.out.println("Added node: " + id + " at (" + xpos + "," + ypos + ")");
+            }
+            
+            // 第二遍：处理所有边的连接
+            for (int i = 0; i < pointList.getLength(); i++) {
+                Element element = (Element) pointList.item(i);
+                Node currentNode = nodeMap.get(element.getElementsByTagName("id").item(0).getTextContent());
+                
+                NodeList neighborInfos = element.getElementsByTagName("NeighbInfo");
+                for (int j = 0; j < neighborInfos.getLength(); j++) {
+                    try {
+                        Element neighborInfo = (Element) neighborInfos.item(j);
+                        NodeList idElements = neighborInfo.getElementsByTagName("id");
+                        for (int k = 0; k < idElements.getLength(); k++) {
+                            Element idElement = (Element) idElements.item(k);
+                            String neighborId = idElement.getTextContent();
+                            
+                            // 检查是否存在isLine属性
+                            boolean isLine = idElement.hasAttribute("isLine") ? 
+                                    "0".equals(idElement.getAttribute("isLine")) : true;
+                                    
+                            Node neighborNode = nodeMap.get(neighborId);
+                            if (neighborNode != null && !graph.hasEdge(currentNode, neighborNode)) {
+                                double distance = Double.parseDouble(
+                                    neighborInfo.getElementsByTagName("distance").item(k).getTextContent()
+                                );
+                                
+                                // 检查是否有控制点
+                                NodeList ctrlPoints = neighborInfo.getElementsByTagName("CtrlPoint");
+                                boolean hasCurve = ctrlPoints.getLength() > 0;
+                                
+                                graph.addEdge(currentNode, neighborNode, distance, 1.0);
+                                /*System.out.println("Added edge: " + currentNode.getId() + " <-> " + 
+                                               neighborNode.getId() + " (distance: " + distance + 
+                                               ", curved: " + hasCurve + ")");*/
+                            }
+                        }
+                    } catch (Exception e) {
+                        System.err.println("Error processing edge: " + e.getMessage());
+                    }
+                }
+            }
+            
+            return graph;
+        } catch (Exception e) {
+            System.err.println("Error loading map: " + e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load map", e);
+        }
+    }
+}
